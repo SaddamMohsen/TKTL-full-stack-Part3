1f87e46ba905531c5d3bce1749e54cbac6b4a48f
SaddamMohsen <sad.2000@outlook.com>
2019-11-26 17:52:37 +0300

    Full-stack phonebook6

---
+++ build/precache-manifest.29193ed781c98ceb81a62a610dc9b1d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82876e05c83ce0cbcaf3bc840f8b8ab2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a57255fb9293b75f2852",
+    "url": "/static/js/2.2cf99cc4.chunk.js"
+  },
+  {
+    "revision": "7edd07c6451d7716a3b5",
+    "url": "/static/js/main.8f73942f.chunk.js"
+  },
+  {
+    "revision": "d352aae964896d31ca48",
+    "url": "/static/js/runtime-main.40f92f37.js"
+  }
+]);
